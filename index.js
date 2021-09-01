a831908ac809b8bdba6a50f4b99a5192f2ad77fc
Joseph <JoeSpanbauer@live.com>
2021-09-01 17:15:18 -0400

    Express API with endpoint that utilizes ts-2-oas package.

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ts2Oas = require('@joespanbauer/ts-2-oas');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Routes
+app.post('/', (req, res) => {
+  let result;
+  try {
+    const { body } = req;
+    result = ts2Oas(body);
+  } catch (e) {
+    result = undefined;
+    res.status(500);
+  }
+
+  if (result) {
+    res.send(result);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 3000');
+});
