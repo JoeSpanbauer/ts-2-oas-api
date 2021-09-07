dce370fd1db53f14046f2efe2d52ade9865a0290
Joseph <JoeSpanbauer@live.com>
2021-09-07 18:40:33 -0400

    Fixing cors issues

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ts2Oas = require('@joespanbauer/ts-2-oas');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const ts2Oas = require('@joespanbauer/ts-2-oas')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
 
 // Routes
 app.post('/', (req, res) => {
-  let result;
+  let result
   try {
-    const { body } = req;
-    result = ts2Oas(body);
+    const { body } = req
+    result = ts2Oas(body)
   } catch (e) {
-    result = undefined;
-    res.status(500);
+    result = undefined
+    res.status(500)
   }
 
   if (result) {
-    res.send(result);
+    res.send(result)
   }
-});
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log('Listening on port 3000');
-});
+  console.log('Listening on port 3000')
+})
